4a3907ff857c8f0b7354aa897cf5eec869c8b5b7
Richard Sun <richardsun29@ucla.edu>
2018-05-22 18:12:04 -0700

    keep track of client connections

--- src/server/server.py
+++ src/server/server.py
@@ -5,60 +5,68 @@ import websockets
 import signal
 import sys
 
-#send_updates_delay = 1.0 / 60
-send_updates_delay = 1.0 / 1
+#SEND_UPDATES_DELAY = 1.0 / 60
+SEND_UPDATES_DELAY = 1.0 / 1
 
-# TODO: class that contains websocket and any other info needed (player name, )
+class Client:
+    def __init__(self, id, websocket):
+        self.id = id
+        self.websocket = websocket
 
-# listen
-async def receive_data(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-        except websockets.exceptions.ConnectionClosed:
-            return
+    async def connect(self):
+        # start sending/receiving
+        recv_task = asyncio.ensure_future(self.receive_data())
+        send_task = asyncio.ensure_future(self.send_data())
+        await asyncio.gather(recv_task, send_task)
 
-        print('receive_data: ' + message + ' ' + str(websocket))
+    async def receive_data(self):
+        # listen
+        while True:
+            message = await self.websocket.recv()
+            print('receive_data: ' + message + ' ' + str(websocket))
 
-
-# send updates
-async def send_data(websocket):
-    i = 1
-    while True:
-        try:
+    async def send_data(self):
+        i = 1
+        # send updates
+        while True:
             message = 'send_data: ' + str(i)
-            await websocket.send(message)
+            await self.websocket.send(message)
+            await asyncio.sleep(SEND_UPDATES_DELAY)
+            i += 1
+
+    def __repr__(self):
+        return 'Client (id = %d)' % (self.id)
+
+
+class Server:
+    def __init__(self):
+        self.clients = set()
+        self.next_id = 1
+
+    def get_new_id(self):
+        self.next_id += 1
+        return self.next_id
+
+    def start_server(self, port):
+        server = websockets.serve(self.handle_connection, 'localhost', port)
+        asyncio.get_event_loop().run_until_complete(server)
+        asyncio.get_event_loop().run_forever()
+        # Stop event loop on ^C
+        signal.signal(signal.SIGINT, asyncio.get_event_loop().close)
+
+    async def handle_connection(self, websocket, path):
+        client = Client(self.get_new_id(), websocket)
+        self.clients.add(client)
+        try:
+            await client.connect()
         except websockets.exceptions.ConnectionClosed:
-            return
-
-        await asyncio.sleep(send_updates_delay)
-        i += 1
-
-
-async def handle_connection(websocket, path):
-    consumer_task = asyncio.ensure_future(receive_data(websocket))
-    producer_task = asyncio.ensure_future(send_data(websocket))
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-
-    for task in pending:
-        task.cancel()
-
-
-def start_server(port):
-    server = websockets.serve(handle_connection, 'localhost', port)
-
-    asyncio.get_event_loop().run_until_complete(server)
-    asyncio.get_event_loop().run_forever()
-
-    # Stop event loop on ^C
-    signal.signal(signal.SIGINT, asyncio.get_event_loop().close)
+            self.clients.remove(client)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 2:
+        server = Server()
+        server.start_server(sys.argv[1])
+    else:
         print('Usage: {} PORT'.format(sys.argv[0]))
         sys.exit(1)
-    start_server(sys.argv[1])
