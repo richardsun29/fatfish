67815f64e7ac70b1b533bfccc7f200f40003c2ec
Richard Sun <richardsun29@ucla.edu>
2018-05-27 05:08:19 -0700

    cleanup canvas layers that no longer exist

--- src/client/js/display.js
+++ src/client/js/display.js
@@ -8,11 +8,19 @@ class Display {
     this.height = 544;
 
     this.drawBackground();
+
+    // occasionally delete old layers
+    setInterval(() => this.cleanup(), 1000);
+  }
+
+  layerName(id) {
+    return `fish${id}`;
   }
 
   drawBackground() {
     this.canvas.drawImage({
       layer: true,
+      name: 'background',
       source: 'img/background.jpg',
       scale: 1,
       x: this.width / 2,
@@ -21,7 +29,7 @@ class Display {
   }
 
   drawFish(id, x, y, size, shadowColor) {
-    var layer = `fish${id}`;
+    var layer = this.layerName(id);
     var scale = size / 10;
 
     x = this.width  * x / 100;
@@ -43,11 +51,44 @@ class Display {
     }
   }
 
+  // hide fish that no longer exist and mark for cleanup
+  hideFish(layer) {
+    if (this.canvas.getLayer(layer)) {
+      this.canvas.setLayer(layer, {
+        x: -100, y: -100,
+        scale: 0,
+        shadowColor: undefined,
+        data: { cleanup: true },
+      });
+    }
+  }
+
   draw(data) {
+    var keepLayer = {'background': true};
+
+    this.drawBackground();
+
+    // draw fish
+    data.players.forEach(p => {
+      this.drawFish(p.id, p.x, p.y, p.size, 'blue');
+      keepLayer[this.layerName(p.id)] = true;
+    });
     data.nonplayers.forEach(p => {
       this.drawFish(p.id, p.x, p.y, p.size, p.id%2 ? 'green': 'red');
+      keepLayer[this.layerName(p.id)] = true;
+    });
+
+    // hide layers that no longer exist
+    this.canvas.getLayers().forEach(layer => {
+      if (!keepLayer[layer.name] && !layer.data.cleanup) {
+        this.hideFish(layer.name);
+      }
     });
 
     this.canvas.drawLayers();
+  }
+
+  cleanup() {
+    this.canvas.removeLayers();
   }
 }
