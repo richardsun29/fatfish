89ec0657c17373630865b192f54f21adb49ab9d3
Richard Sun <richardsun29@ucla.edu>
2018-05-27 22:50:21 -0700

    much cleaner cleanup code

--- src/client/js/display.js
+++ src/client/js/display.js
@@ -8,13 +8,6 @@ class Display {
     this.height = 544;
 
     this.drawBackground();
-
-    // occasionally delete old layers
-    setInterval(() => this.cleanup(), 1000);
-  }
-
-  layerName(id) {
-    return `fish${id}`;
   }
 
   drawBackground() {
@@ -29,66 +22,32 @@ class Display {
   }
 
   drawFish(id, x, y, size, shadowColor) {
-    var layer = this.layerName(id);
+    var layer = `fish${id}`;
     var scale = size / 10;
 
     x = this.width  * x / 100;
     y = this.height * y / 100;
 
-    // check if layer exists
-    if (this.canvas.getLayer(layer)) {
-      // change layer
-      this.canvas.setLayer(layer, {x, y, scale, shadowColor});
-    }
-    else {
-      // create new layer
-      this.canvas.drawImage({x, y, scale, shadowColor,
-        layer: true,
-        name: layer,
-        source: 'img/fish.png',
-        shadowBlur: 20
-      });
-    }
-  }
-
-  // hide fish that no longer exist and mark for cleanup
-  hideFish(layer) {
-    if (this.canvas.getLayer(layer)) {
-      this.canvas.setLayer(layer, {
-        x: -100, y: -100,
-        scale: 0,
-        shadowColor: undefined,
-        data: { cleanup: true },
-      });
-    }
+    this.canvas.drawImage({x, y, scale, shadowColor,
+      layer: true,
+      name: layer,
+      source: 'img/fish.png',
+      shadowBlur: 20
+    });
   }
 
   draw(data) {
-    var keepLayer = {'background': true};
-
+    this.canvas.removeLayers();
     this.drawBackground();
 
     // draw fish
     data.players.forEach(p => {
       this.drawFish(p.id, p.x, p.y, p.size, 'blue');
-      keepLayer[this.layerName(p.id)] = true;
     });
     data.nonplayers.forEach(p => {
       this.drawFish(p.id, p.x, p.y, p.size, p.id%2 ? 'green': 'red');
-      keepLayer[this.layerName(p.id)] = true;
-    });
-
-    // hide layers that no longer exist
-    this.canvas.getLayers().forEach(layer => {
-      if (!keepLayer[layer.name] && !layer.data.cleanup) {
-        this.hideFish(layer.name);
-      }
     });
 
     this.canvas.drawLayers();
-  }
-
-  cleanup() {
-    this.canvas.removeLayers();
   }
 }
